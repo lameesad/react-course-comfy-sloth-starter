e2d78381feed7b4be4522004135acb098cb850ae
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-05-18 12:10:52 +0300

    Home page, Services

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,22 @@ import heroBcg from '../assets/hero-bcg.jpeg'
 import heroBcg2 from '../assets/hero-bcg-2.jpeg'
 
 const Hero = () => {
-  return <h4>hero</h4>
+  return <Wrapper className="section-center">
+    <article className="content">
+      <h1>design your <br />
+          comfort Zone
+      </h1>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+      <Link to='/products' className="btn hero-btn">shop now</Link>
+    </article>
+    <article className="img-container">
+      <img src={heroBcg} alt="nice table" className="main-img" />
+      <img src={heroBcg2} alt="person working" className="accent-img" />
+    </article>
+
+  </Wrapper>
 }
 
 const Wrapper = styled.section`
