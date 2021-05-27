8731a36f946c71518073024690793349507f5902
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-05-27 15:07:42 +0300

    Cart Item

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -4,16 +4,47 @@ import { formatPrice } from '../utils/helpers'
 import AmountButtons from './AmountButtons'
 import { FaTrash } from 'react-icons/fa'
 import { useCartContext } from '../context/cart_context'
-const CartItem = () => {
-  return <h4>cart item</h4>
+const CartItem = ({ id, image, name, color, price, amount }) => {
+  const { removeItem, toggleAmount } = useCartContext()
+  const increase = () => {
+
+  }
+  const decrease = () => {
+
+  }
+  return <Wrapper>
+    <div className="title">
+      <img src={image} alt={name} />
+      <div>
+        <h5 className="name">{name}</h5>
+        <p className="color">
+          color : <span style={{ background: color }}></span>
+        </p>
+        <h5 className="price-small">{formatPrice(price)}</h5>
+      </div>
+    </div>
+    <h5 className="price">{formatPrice(price)}</h5>
+    <AmountButtons
+      amount={amount}
+      increase={increase}
+      decrease={decrease}
+    />
+    <h5 className="subtotal">{formatPrice(price * amount)}</h5>
+    <button
+      type="button"
+      className="remove-btn"
+      onClick={() => { removeItem(id) }}>
+      <FaTrash />
+    </button>
+  </Wrapper>
 }
 
 const Wrapper = styled.article`
   .subtotal {
-    display: none;
+      display: none;
   }
   .price {
-    display: none;
+      display: none;
   }
   display: grid;
   grid-template-columns: 200px auto auto;
@@ -23,7 +54,7 @@ const Wrapper = styled.article`
   margin-bottom: 3rem;
   align-items: center;
   .title {
-    grid-template-rows: 75px;
+      grid - template - rows: 75px;
     display: grid;
     grid-template-columns: 75px 125px;
     align-items: center;
@@ -31,19 +62,19 @@ const Wrapper = styled.article`
     gap: 1rem;
   }
   img {
-    width: 100%;
+      width: 100%;
     height: 100%;
     display: block;
     border-radius: var(--radius);
     object-fit: cover;
   }
   h5 {
-    font-size: 0.75rem;
+      font - size: 0.75rem;
     margin-bottom: 0;
   }
 
   .color {
-    color: var(--clr-grey-5);
+      color: var(--clr-grey-5);
     font-size: 0.75rem;
     letter-spacing: var(--spacing);
     text-transform: capitalize;
@@ -61,21 +92,21 @@ const Wrapper = styled.article`
     }
   }
   .price-small {
-    color: var(--clr-primary-5);
+      color: var(--clr-primary-5);
   }
   .amount-btns {
-    width: 75px;
+      width: 75px;
     button {
       width: 1rem;
       height: 0.5rem;
       font-size: 0.75rem;
     }
     h2 {
-      font-size: 1rem;
+      font - size: 1rem;
     }
   }
   .remove-btn {
-    color: var(--clr-white);
+      color: var(--clr-white);
     background: transparent;
     border: transparent;
     letter-spacing: var(--spacing);
@@ -107,12 +138,12 @@ const Wrapper = styled.article`
       font-weight: 400;
     }
     .name {
-      font-size: 0.85rem;
+      font - size: 0.85rem;
     }
     .color {
-      font-size: 0.85rem;
+      font - size: 0.85rem;
       span {
-        width: 0.75rem;
+      width: 0.75rem;
         height: 0.75rem;
       }
     }
@@ -133,12 +164,12 @@ const Wrapper = styled.article`
     .amount-btns {
       width: 100px;
       button {
-        width: 1.5rem;
+      width: 1.5rem;
         height: 1rem;
         font-size: 1rem;
       }
       h2 {
-        font-size: 1.5rem;
+      font - size: 1.5rem;
       }
     }
   }
